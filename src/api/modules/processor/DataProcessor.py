82fcff7c43d00246f4c47959c718971c50024d15
Olesia <melnichenko.o@gmail.com>
2025-08-10 09:56:36 +0300

    H2H stats revised

--- src/api/modules/processor/DataProcessor.py
+++ src/api/modules/processor/DataProcessor.py
@@ -1,3 +1,4 @@
+from tokenize import group
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 import logging
@@ -14,32 +15,6 @@ class DataProcessor:
         """
         pass
 
-    def add_h2h_stats(self, group, current_date = '2024-07-01'):
-        groups_by_opponent = group.groupby('opponent')
-        for h2h_group_name in groups_by_opponent.groups:
-            h2h_group = groups_by_opponent.get_group(h2h_group_name)
-            h2h_group = h2h_group[h2h_group['Date'] < current_date]
-            # group.iloc[group.opponent == h2h_group_name, group.columns.get_loc("h2h_win")] = h2h_group['Win'].mean()
-            # group.iloc[group.opponent == h2h_group_name, group.columns.get_loc("h2h_loss")] = h2h_group['Loss'].mean()
-            groups_by_venue = h2h_group.groupby('venue')
-            venues = list(groups_by_venue.groups.keys())
-            if len(venues) == 1:
-                if 1 - venues[0] == 0: # missing value
-                    group.iloc[group.opponent == h2h_group_name, group.columns.get_loc("h2h_home_win")] = 0.5
-                    group.iloc[group.opponent == h2h_group_name, group.columns.get_loc("h2h_home_loss")] = 0.5
-                else:
-                    group.iloc[group.opponent == h2h_group_name, group.columns.get_loc("h2h_guest_win")] = 0.5
-                    group.iloc[group.opponent == h2h_group_name, group.columns.get_loc("h2h_guest_loss")] = 0.5
-            for venue in venues:
-                venue_group = groups_by_venue.get_group(venue)
-                if venue == 0:
-                    group.iloc[group.opponent == h2h_group_name, group.columns.get_loc("h2h_home_win")] = venue_group['Win'].mean()
-                    group.iloc[group.opponent == h2h_group_name, group.columns.get_loc("h2h_home_loss")] = venue_group['Loss'].mean()
-                else:
-                    group.iloc[group.opponent == h2h_group_name, group.columns.get_loc("h2h_guest_win")] = venue_group['Win'].mean()
-                    group.iloc[group.opponent == h2h_group_name, group.columns.get_loc("h2h_guest_loss")] = venue_group['Loss'].mean()
-        return group
-    
     def add_averages(self, group, cols, windows):
         group = group.sort_values("Date")
         
@@ -51,12 +26,42 @@ class DataProcessor:
         
         return group
     
+    def add_h2h_stats(self, group):
+        opponents_grouped = group.groupby('opponent')
+        for opponent, h2h_df in opponents_grouped:
+            h2h_df = h2h_df.sort_values('Date')
+            
+            for index, row in h2h_df.iterrows():
+                prev_matches = h2h_df[h2h_df['Date'] < row.Date]
+                prev_matches_home = prev_matches[prev_matches['venue'] == 0]
+                prev_matches_guest = prev_matches[prev_matches['venue'] == 1]
+
+                group.loc[group['Date'] == row.Date, 'h2h_home_win'] = 0
+                group.loc[group['Date'] == row.Date, 'h2h_home_draw'] = 0
+                group.loc[group['Date'] == row.Date, 'h2h_home_loss'] = 0
+                group.loc[group['Date'] == row.Date, 'h2h_guest_win'] = 0
+                group.loc[group['Date'] == row.Date, 'h2h_guest_draw'] = 0
+                group.loc[group['Date'] == row.Date, 'h2h_guest_loss'] = 0
+                row[['h2h_home_win', 'h2h_home_draw', 'h2h_home_loss', 
+                     'h2h_guest_win', 'h2h_guest_draw', 'h2h_guest_loss']] = pd.Series([0, 0, 0, 0, 0, 0])
+                
+                if prev_matches_home.shape[0] > 0:
+                    group.loc[group['Date'] == row.Date, 'h2h_home_win'] = prev_matches_home['Win'].mean()
+                    group.loc[group['Date'] == row.Date, 'h2h_home_draw'] = prev_matches_home['Draw'].mean()
+                    group.loc[group['Date'] == row.Date, 'h2h_home_loss'] = prev_matches_home['Loss'].mean()
+                if prev_matches_guest.shape[0] > 0:
+                    group.loc[group['Date'] == row.Date, 'h2h_guest_win'] = prev_matches_guest['Win'].mean()
+                    group.loc[group['Date'] == row.Date, 'h2h_guest_draw'] = prev_matches_guest['Draw'].mean()
+                    group.loc[group['Date'] == row.Date, 'h2h_guest_loss'] = prev_matches_guest['Loss'].mean()
+
+        return group
+    
     def filter_dataset_4_stats(self, data, upcoming_matches_df):
         """
         Filter the dataset to include only matches that are relevant for statistics.
         """
         teams_set = set(upcoming_matches_df['HomeTeam']) | set(upcoming_matches_df['AwayTeam'])
-        df, cols_4_avg = self.get_df_transformed(data, None, add_stats=False)  # Get DataFrame without additional stats
+        df, cols_4_avg = self.get_df_transformed(data, add_stats=False)  # Get DataFrame without additional stats
         df = df.sort_values(by='Date', ascending = False)
 
         last_matches = { team : df[df['team'] == team].head(15) for team in teams_set}
@@ -68,7 +73,7 @@ class DataProcessor:
 
         return last_matches, h2h_matches, cols_4_avg
     
-    def get_df_transformed(self, data, current_date, add_stats = True):
+    def get_df_transformed(self, data, add_stats = True):
         df = pd.DataFrame(data)
         # cols = df.columns.tolist()
         # Display basic information
@@ -125,9 +130,10 @@ class DataProcessor:
         if add_stats:
             # Add head 2 head statistics
             df_with_h2h = df.reindex(columns=df.columns.tolist() + 
-                                    ['h2h_home_win', 'h2h_home_loss', 'h2h_guest_win', 'h2h_guest_loss']) # 'h2h_win', 'h2h_loss',
+                                    ['h2h_home_win', 'h2h_home_draw', 'h2h_home_loss', 
+                                     'h2h_guest_win', 'h2h_guest_draw', 'h2h_guest_loss']) # 'h2h_win', 'h2h_loss',
 
-            df_with_h2h = df_with_h2h.groupby('team').apply(lambda x: self.add_h2h_stats(x, current_date))
+            df_with_h2h = df_with_h2h.groupby('team').apply(lambda x: self.add_h2h_stats(x))
             df_with_h2h = df_with_h2h.droplevel('team')
             df_with_h2h.index = range(df_with_h2h.shape[0])
             
@@ -151,7 +157,7 @@ class DataProcessor:
         if current_date is None:
             current_date = '2024-07-01'
 
-        df_with_avg, cols_4_avg = self.get_df_transformed(data, current_date)
+        df_with_avg, cols_4_avg = self.get_df_transformed(data)
         df_with_avg = df_with_avg.fillna(0)
 
         le = LabelEncoder()
@@ -163,7 +169,7 @@ class DataProcessor:
         test = df_with_avg[df_with_avg['Date'] > current_date]
 
         predictors = ['venue', # , 'opp_code', 'team_code'
-              'h2h_home_win', 'h2h_home_loss', 'h2h_guest_win', 'h2h_guest_loss']
+              'h2h_home_win', 'h2h_home_draw', 'h2h_home_loss', 'h2h_guest_win', 'h2h_guest_draw', 'h2h_guest_loss']
         predictors += [f"{col}_avg{window}" for window in [5, 10, 15] for col in cols_4_avg]
 
         print(f"Predictors count: {len(predictors)}")
@@ -183,8 +189,8 @@ class DataProcessor:
 
         return X_train, X_test, y_train, y_test
 
-    def get_samples_to_predict(self, data, filename = './data/futur_matches.csv') :
-        df = pd.read_csv(filename)
+    def get_samples_to_predict_from_json(self, data, json_data) :
+        df = pd.read_json(json_data)
         df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%Y')
         last_matches, h2h_matches, cols_4_avg = self.filter_dataset_4_stats(data, df)
         samples = []
@@ -193,14 +199,18 @@ class DataProcessor:
             h2h_home = h2h_df[h2h_df['venue'] == 0]
             h2h_guest = h2h_df[h2h_df['venue'] == 1]
             samples.append({'team': team, 'opponent': opponent, 'venue': 0, 
-                            'h2h_home_win': 0.5 if h2h_home.shape[0] == 0 else h2h_home['Win'].mean(), 
+                            'h2h_home_win': 0.5 if h2h_home.shape[0] == 0 else h2h_home['Win'].mean(),
+                            'h2h_home_draw': 0 if h2h_home.shape[0] == 0 else h2h_home['Draw'].mean(), 
                             'h2h_home_loss': 0.5 if h2h_home.shape[0] == 0 else h2h_home['Loss'].mean(),
-                            'h2h_guest_win': 0.5 if h2h_guest.shape[0] == 0 else h2h_guest['Win'].mean(), 
+                            'h2h_guest_win': 0.5 if h2h_guest.shape[0] == 0 else h2h_guest['Win'].mean(),
+                            'h2h_guest_draw': 0 if h2h_guest.shape[0] == 0 else h2h_guest['Draw'].mean(),
                             'h2h_guest_loss': 0.5 if h2h_guest.shape[0] == 0 else h2h_guest['Loss'].mean()})
             samples.append({'team': opponent, 'opponent': team, 'venue': 1, 
-                            'h2h_home_win': 0.5 if h2h_guest.shape[0] == 0 else h2h_guest['Loss'].mean(), 
+                            'h2h_home_win': 0.5 if h2h_guest.shape[0] == 0 else h2h_guest['Loss'].mean(),
+                            'h2h_home_draw': 0 if h2h_guest.shape[0] == 0 else h2h_guest['Draw'].mean(), 
                             'h2h_home_loss': 0.5 if h2h_guest.shape[0] == 0 else h2h_guest['Win'].mean(),
-                            'h2h_guest_win': 0.5 if h2h_home.shape[0] == 0 else h2h_home['Loss'].mean(), 
+                            'h2h_guest_win': 0.5 if h2h_home.shape[0] == 0 else h2h_home['Loss'].mean(),
+                            'h2h_guest_draw': 0 if h2h_home.shape[0] == 0 else h2h_home['Draw'].mean(),
                             'h2h_guest_loss': 0.5 if h2h_home.shape[0] == 0 else h2h_home['Win'].mean()})
         for team, last_match in last_matches.items():
             for dict in list(filter(lambda x: x['team'] == team, samples)):
